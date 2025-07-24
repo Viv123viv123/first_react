e6bd5478bed2b7b5ae2d7c3422f98785a8bb1a62
vivek kumar <vishalroy8482@gmail.com>
2025-07-24 16:57:39 +0530

    next

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ console.log("This is a script file located at git04/script.js");
 
 console.log("This is a script file located at git04/script.js");
 
-
-
+console.log("hELLO vivek"); // IGNORE
+console.log("dignd");
